9215b70a2073303ca867da752ba02f068b05dc32
Курганов Никита <110167907+sooriken@users.noreply.github.com>
2024-12-25 20:07:30 +0300

    Big update

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -6,3 +6,31 @@ import 'air-datepicker/air-datepicker.css';
 new AirDatepicker('#date');
 
 console.log(module);
+
+var faqItems = document.getElementsByClassName("faq__item");
+console.log(faqItems);
+
+faqItems[2].onclick = function() {
+  changeFaq(2);
+};
+
+faqItems[1].onclick = function() {
+  changeFaq(1);
+};
+
+faqItems[0].onclick = function() {
+  changeFaq(0);
+};
+
+function changeFaq(faqID) {
+    console.log(faqItems[faqID]);
+    console.log(faqID);
+    var i = 0;
+    while(i < faqItems.length) {
+        faqItems[i].classList.remove("faq__item--active");
+        console.log(i);
+        i++;
+    }
+    console.log("stop");
+    faqItems[faqID].classList.add("faq__item--active");
+}
